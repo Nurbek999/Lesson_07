f76b9d56a45c3ff91abfa5c8c5f33ecde38ac05b
Amanbek Nurbek <nurbek_aman@mail.ru>
2022-04-03 11:41:12 +0600

    Исправил ошибку

--- task02.js
+++ task02.js
@@ -3,7 +3,7 @@
 const allСashbox = [4500, 3210, 650, 1250, 7830, 990, 13900, 370];
 
 const getAverageValue = (arr) => {
-    var sum = 0;
+    let sum = 0;
     for(var i = 0; i < arr.length; i++) {
         sum += arr[i];
     }
